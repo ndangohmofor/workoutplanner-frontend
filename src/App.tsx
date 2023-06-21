686f50a70597f375f122b06c0358db8c93c823ba
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2023-06-20 22:44:48 -0400

    Created the navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Register from "./components/Register";
-import { Navbar } from "react-bootstrap";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Register />
     </div>
   );
 }
